a5772d0307cb678f7bf818a04141b745e51521fa
Amir Maso'ud Mehrabian <aut0run2011@gmail.com>
2026-02-21 19:25:49 +0330

    Fixed a minor issue

--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,7 @@ Route::middleware(['auth', 'initialize_tenancy', 'check_tenant_status'])->group(
         Route::get('/', [UserController::class, 'index'])->name('index');
         Route::get('/create', [UserController::class, 'create'])->name('create');
         Route::post('/', [UserController::class, 'store'])->name('store');
+        Route::get('/{user}', [UserController::class, 'show'])->name('show');
         Route::get('/{user}/edit', [UserController::class, 'edit'])->name('edit');
         Route::put('/{user}', [UserController::class, 'update'])->name('update');
         Route::delete('/{user}', [UserController::class, 'destroy'])->name('destroy');
