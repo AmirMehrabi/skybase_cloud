6371e8366602c3fc7345381a40bb3f447defa8c1
Amir Maso'ud Mehrabian <aut0run2011@gmail.com>
2026-03-18 13:17:20 +0330

    Fixing a minor issue with routes

--- resources/views/routers/index.blade.php
+++ resources/views/routers/index.blade.php
@@ -205,13 +205,13 @@
                             </td>
                             <td class="px-6 py-4">
                                 <div class="flex items-center justify-end gap-2">
-                                    <a :href="`{{ route('routers.show', '') }}/${router.id}`" class="p-1.5 text-gray-400 hover:text-blue-600 hover:bg-blue-50 rounded-lg transition-colors" title="View">
+                                    <a :href="urls.show + '/' + router.id" class="p-1.5 text-gray-400 hover:text-blue-600 hover:bg-blue-50 rounded-lg transition-colors" title="View">
                                         <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path>
                                         </svg>
                                     </a>
-                                    <a :href="`{{ route('routers.edit', '') }}/${router.id}`" class="p-1.5 text-gray-400 hover:text-green-600 hover:bg-green-50 rounded-lg transition-colors" title="Edit">
+                                    <a :href="urls.edit + '/' + router.id" class="p-1.5 text-gray-400 hover:text-green-600 hover:bg-green-50 rounded-lg transition-colors" title="Edit">
                                         <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path>
                                         </svg>
@@ -316,6 +316,11 @@ function routersIndex() {
             deleting: false
         },
         debounceTimer: null,
+        urls: {
+            show: '{{ url('routers') }}',
+            edit: '{{ url('routers') }}',
+            destroy: '{{ url('routers') }}'
+        },
 
         init() {
             this.loadStats();
@@ -394,7 +399,7 @@ function routersIndex() {
 
             this.deleteModal.deleting = true;
             try {
-                const response = await fetch(`{{ route('routers.index') }}/${this.deleteModal.router.id}`, {
+                const response = await fetch(`${this.urls.destroy}/${this.deleteModal.router.id}`, {
                     method: 'DELETE',
                     headers: {
                         'X-CSRF-TOKEN': '{{ csrf_token() }}',
