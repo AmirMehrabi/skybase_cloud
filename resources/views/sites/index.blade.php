6e8c74a4ab089a712243ede8885443b29592edfa
Amir Maso'ud Mehrabian <aut0run2011@gmail.com>
2026-05-26 01:10:43 +0330

    Fixed a minor issue

--- resources/views/sites/index.blade.php
+++ resources/views/sites/index.blade.php
@@ -125,11 +125,11 @@
         <div x-show="loading" class="px-6 py-12 text-center text-sm text-gray-500" style="display: none;">Loading sites...</div>
     </div>
 
-    <div x-show="deleteModal.show" class="fixed inset-0 z-50 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true" style="display: none;">
-        <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-            <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+    <div x-show="deleteModal.show" class="fixed inset-0 z-[2000] overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true" style="display: none;">
+        <div class="relative z-[2000] flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            <div class="fixed inset-0 z-[2000] bg-gray-500 bg-opacity-75 transition-opacity"></div>
             <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
-            <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+            <div class="relative z-[2010] inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
                 <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                     <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-title">Delete Site</h3>
                     <p class="text-sm text-gray-500 mt-2">Are you sure you want to delete "<span x-text="deleteModal.site?.name"></span>"? Assigned routers will keep their legacy site text but lose this managed site link.</p>
